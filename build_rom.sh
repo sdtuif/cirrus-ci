d6b166cef4d5259dd8fd57d4a417a3d15e3a2cd0
sdtuif <118095651+sdtuif@users.noreply.github.com>
2023-02-07 22:18:22 +0600

    lavender-nusantara-psycho

---
+++ build_rom.sh
@@ -0,0 +1,16 @@
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/Sa-Sajjad/android_manifest_nusa.git -b 10 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Sa-Sajjad/manifest.git --depth 1 -b main .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+source build/envsetup.sh
+lunch nad_lavender-userdebug
+export TZ=Asia/Dhaka
+make api-stubs-docs || echo no problem
+make system-api-stubs-docs || echo no problem
+make test-api-stubs-docs || echo no problem
+mka nad
+
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
