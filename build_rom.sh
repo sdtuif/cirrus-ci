6189c236e1c648f92f154393f7cf40ff970f20f1
sdtuif <118095651+sdtuif@users.noreply.github.com>
2023-02-08 21:58:16 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,14 @@
 # sync rom
+echo "=== Repo Initialization ==="
 repo init --depth=1 --no-repo-verify -u https://github.com/Sa-Sajjad/android_manifest_nusa.git -b backup -g default,-mips,-darwin,-notdefault
+echo "=== Clone DT VT & VT ==="
 git clone https://github.com/Sa-Sajjad/manifest.git --depth 1 -b main .repo/local_manifests
+echo "=== Repo Sync ==="
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+echo "=== Repo Sync Complete ==="
 
 # build rom
+echo "=== Run Build Cmd ==="
 source build/envsetup.sh
 lunch nad_lavender-userdebug
 export TZ=Asia/Dhaka
