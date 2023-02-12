88bd27b9db0caf00b9e740795598bf603abd1be4
sdtuif <118095651+sdtuif@users.noreply.github.com>
2023-02-12 13:41:20 +0600

    lavender-nusantara-psycho

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 echo "=== Repo Initialization ==="
-repo init --depth=1 --no-repo-verify -u https://github.com/Sa-Sajjad/android_manifest_nusa.git -b backup -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Sa-Sajjad/android_manifest_nusa.git -b test-gif -g default,-mips,-darwin,-notdefault
 echo "=== Clone DT VT & VT ==="
 git clone https://github.com/Sa-Sajjad/manifest.git --depth 1 -b 1 .repo/local_manifests
 echo "=== Repo Sync ==="
