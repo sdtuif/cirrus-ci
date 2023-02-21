9a558ad40b5f2578348d07d9e5388e75e7223138
sdtuif <118095651+sdtuif@users.noreply.github.com>
2023-02-21 20:40:45 +0600

    Clean up

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,10 @@
-# sync rom
-echo "=== Repo Initialization ==="
-repo init --depth=1 --no-repo-verify -u https://github.com/Snax-phycho/android_manifest_nusa.git -b 10 -g default,-mips,-darwin,-notdefault
-echo "=== Clone DT VT & VT ==="
+# add repo,dt, vt, kt etc
+repo init --depth=1 --no-repo-verify -u https://github.com/Sa-Sajjad/android_manifest_nusa.git -b 10 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Sa-Sajjad/manifest.git --depth 1 -b 1 .repo/local_manifests
-echo "=== Repo Sync ==="
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-echo "=== Repo Sync Complete ==="
 
 # build rom
-echo "=== Run Build Cmd ==="
+# don't touch code
 source build/envsetup.sh
 lunch nad_lavender-userdebug
 export TZ=Asia/Dhaka
@@ -16,6 +12,4 @@ make api-stubs-docs || echo no problem
 make system-api-stubs-docs || echo no problem
 make test-api-stubs-docs || echo no problem
 mka nad
-
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
